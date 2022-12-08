5959e0913caede260f65838aab7a94f9eb84a17a
cheppretty <cheppretty4@gmail.com>
2022-12-08 11:42:54 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
-
-
-    /**  
- *
- *    * main - Entry point
- *    * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
-int main  (void)
-
+/**
+ * main-print to screen
+ * Return: 0
+ */
+int main(void)
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
 }
+
