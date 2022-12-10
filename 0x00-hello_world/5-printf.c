ca31a733298d5fa44bba8caa91ebb875fd2241ea
cheppretty <cheppretty4@gmail.com>
2022-12-10 00:56:35 -0800

    5prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
-  *main - *Entry    
-  *Return: 0 Always 0 (Success) 
-  */
-
+*main - *Entry    
+*Return: 0 Always 0 (Success) 
+*/
 int main(void)
-
 {
-
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-     return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
