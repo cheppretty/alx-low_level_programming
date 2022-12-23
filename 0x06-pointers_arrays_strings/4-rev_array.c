81989920c7d60882466b4c70183ae070730e5d7a
cheppretty <cheppretty4@gmail.com>
2022-12-22 23:00:59 -0800

    four

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,4 +3,16 @@
 *reverse_array - reverse array of integers
 *@a: array
 *@n: number of elements of array
-*Return:void  
+*Return:void 
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+int t;
+for (i = 0; i < n--; i++)
+{
+t = a[i];
+a[i] = a[n];
+a[n] = t;
+}
+}
